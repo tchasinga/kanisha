180696070eabacb9a664573eb768a1183d3f8896
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-24 12:34:01 +0200

    new code is added now

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { View , Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 
-export default function componentName() {
+
+
+export default function HomeScreen() {
   return (
     <SafeAreaView>
-        <View>
-        <Text></Text>
-        </View>
-        <StatusBar style='auto'/>
-     </SafeAreaView>
+    <View className='text-lg'>
+      <Text>The best is comming now</Text>
+    </View>
+    <StatusBar style='auto'/>
+    </SafeAreaView>
   );
 }
