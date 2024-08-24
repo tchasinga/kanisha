f1ceb7f74fae8b1f23cd6cc9eeddf2cd4a81c0ce
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-24 13:21:10 +0200

    Adding new linear gradient

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,8 @@
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+// import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import { Slot } from 'expo-router';
 
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -23,9 +24,6 @@ export default function RootLayout() {
   }
 
   return (
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+     <Slot/>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,15 @@
-import { View , Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-
-
+import BImage from "@/assets/meditation-images/beach.webp";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-    <View className='text-lg'>
-      <Text>The best is comming now</Text>
-    </View>
-    <StatusBar style='auto'/>
+    <SafeAreaView className="flex-1">
+      <ImageBackground source={BImage} resizeMode="cover" className="flex-1 justify-center items-center">
+        <Text className="text-black text-lg">The best is coming now</Text>
+      </ImageBackground>
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
