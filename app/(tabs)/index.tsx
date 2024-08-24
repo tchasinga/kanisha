a4efea28f98dc1448db7a3a8037d7f1614f601fd
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-24 10:48:50 +0200

    new code is added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,7 @@
 import { View , Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+
 
 
 export default function HomeScreen() {
@@ -8,6 +10,7 @@ export default function HomeScreen() {
     <View className='text-lg'>
       <Text>The best is comming now</Text>
     </View>
+    <StatusBar style='auto'/>
     </SafeAreaView>
   );
 }
