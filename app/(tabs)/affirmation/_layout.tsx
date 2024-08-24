151465b1ead24b2d431cc3fe4f4b1b6779870c70
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-24 17:39:29 +0200

    new design code is added now

---
+++ app/(tabs)/affirmation/_layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Stack } from "expo-router";
+
+const AffirmationsLayout = () => {
+    return (
+        <Stack>
+            <Stack.Screen
+                name="index"
+                options={{
+                    headerShown: false,
+                }}
+            />
+        </Stack>
+    );
+};
+
+export default AffirmationsLayout;
