9da37ee017390e8faaa6a216db29f1d9be503ade
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-24 13:58:23 +0200

    new code is commited

--- app/index.tsx
+++ app/index.tsx
@@ -9,12 +9,16 @@ export default function HomeScreen() {
     <SafeAreaView className="flex-1">
       <ImageBackground source={BImage} resizeMode="cover" className="flex-1">
         <LinearGradient className='flex-1' colors={["rgba(0,0,0, 0.4)", "rgba(0,0,0, 0.8)"]}>
+          <SafeAreaView className='flex-1 justify-between px-1'>
             <View>
-            <Text className="text-white text-center font-bold text-3xl">Zua tango</Text>
+              <Text className="text-white text-center font-bold text-3xl">Zua tango</Text>
+                 <Text className='text-white text-center text-sm'>Relax & take note, more you read more you learn</Text>
             </View>
-        </LinearGradient>      
+
+            <StatusBar style="dark" />
+          </SafeAreaView>
+        </LinearGradient>
       </ImageBackground>
-      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
