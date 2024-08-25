f68fc2245c590f0cf39bf7370a6089f1c33feba1
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-25 11:59:19 +0200

    chore: Remove unused code and dependencies in _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,13 @@
 import { useFonts } from 'expo-font';
 // import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import { Slot, Stack } from 'expo-router';
 
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/RobotoMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+  
   return (
     <Stack>
       <Stack.Screen name='(tabs)' options={{headerShown: false}}/>
