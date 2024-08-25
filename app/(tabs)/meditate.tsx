4584bae912bc9eb59f2d032394fed10ad34ba463
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-25 12:33:00 +0200

    chore: Add back button to Meditate tab in meditate.tsx

--- app/(tabs)/meditate.tsx
+++ app/(tabs)/meditate.tsx
@@ -1,8 +1,10 @@
-import { View, Text, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, Pressable } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
 import meditationImage from '@/constants/fileformeditation'
 import AppGradiant from '@/components/Appgradient'
+import { router } from 'expo-router'
+import { AntDesign } from '@expo/vector-icons'
 
 const Meditate = () => {
   return (
@@ -10,6 +12,9 @@ const Meditate = () => {
         <ImageBackground source={meditationImage[0]} className='flex-1' resizeMode='cover'> 
             <AppGradiant colors={["rgba(0,0,0, 0.4)", "rgba(0,0,0, 0.8)"]}>
             <Text className='text-white text-2xl text-center'>Meditate</Text>
+            <Pressable onPress={() => router.back()} className='absolute top-16 left-6 z-10 bg-white rounded-full p-2 shadow-md'>
+              <AntDesign name="back" size={24} color="black" />
+            </Pressable>
             </AppGradiant>
         </ImageBackground>
     </SafeAreaView>
