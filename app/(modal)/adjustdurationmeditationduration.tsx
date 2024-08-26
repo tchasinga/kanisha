b3715ba616fed101b2bf19c0341b7083499f58a7
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-26 08:18:55 +0200

    refactor: Add Adjustdurationmeditationduration modal component

---
+++ app/(modal)/adjustdurationmeditationduration.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Adjustdurationmeditationduration = () => {
+  return (
+    <View>
+      <Text>adjustdurationmeditationduration</Text>
+    </View>
+  )
+}
+
+export default Adjustdurationmeditationduration
