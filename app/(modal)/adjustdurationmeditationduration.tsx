5465f140c82cee5082216d1989021ce9c3c86420
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-26 08:29:01 +0200

    refactor: Add Adjustdurationmeditationduration modal component

--- app/(modal)/adjustdurationmeditationduration.tsx
+++ app/(modal)/adjustdurationmeditationduration.tsx
@@ -1,12 +1,64 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable } from 'react-native'
+import React, { useContext } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import AppGradient from '@/components/Appgradient';
+import { router } from 'expo-router';
+import { AntDesign } from '@expo/vector-icons';
+import CustomButtom from '@/components/CustomButtom';
 
 const Adjustdurationmeditationduration = () => {
+
+    const { setDuration } = useContext(TimerContext);
+
+    const handlePress = (duration: number) => {
+        setDuration(duration);
+        router.back();
+    };
+
   return (
-    <SafeAreaView>
-      <Text>adjustdurationmeditationduration</Text>
-    </SafeAreaView>
+    <View className="flex-1 relative">
+            <AppGradient
+                // Background Linear Gradient
+                colors={["#161b2e", "#0a4d4a", "#766e67"]}
+            >
+                <Pressable
+                    onPress={() => router.back()}
+                    className="absolute top-8 left-6 z-10"
+                >
+                    <AntDesign name="leftcircleo" size={50} color="white" />
+                </Pressable>
+                <View className="justify-center h-4/5">
+                    <View>
+                        <Text className="text-center font-bold text-3xl text-white mb-8">
+                            Adjust your meditation duration
+                        </Text>
+                    </View>
+
+                    <View>
+                        <CustomButtom
+                            title="10 seconds"
+                            onPress={() => handlePress(10)}
+                            containerStyles="mb-5"
+                        />
+                        <CustomButtom
+                            title="5 minutes"
+                            onPress={() => handlePress(5 * 60)}
+                            containerStyles="mb-5"
+                        />
+                        <CustomButtom
+                            title="10 minutes"
+                            onPress={() => handlePress(10 * 60)}
+                            containerStyles="mb-5"
+                        />
+                        <CustomButtom
+                            title="15 minutes"
+                            onPress={() => handlePress(15 * 60)}
+                            containerStyles="mb-5"
+                        />
+                    </View>
+                </View>
+            </AppGradient>
+        </View>
   )
 }
 
