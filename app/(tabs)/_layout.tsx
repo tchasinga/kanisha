5f43cded206a7010c28f721cf741b4df77812644
Tchasinga jacques <tchasingajacques@gmail.com>
2025-01-22 12:54:10 +0300

    refactor: Update import path for Color constant and expand Tailwind CSS content paths to include screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Tabs } from 'expo-router'
-import Color from '@/constants/Color'
+import Color from '../../constants/Color';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
