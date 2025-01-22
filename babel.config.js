c02e0ec995a7e68768ac098d752251f4aa534b39
Tchasinga jacques <tchasingajacques@gmail.com>
2025-01-22 11:48:44 +0300

    refactor: Add react-native-reanimated plugin and update @types/react-native for compatibility

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,17 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"],
+    plugins: [
+      "nativewind/babel",
+      'react-native-reanimated/plugin',
+    ],
   };
+};
+
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    // Add any required plugins here, for example:
+    
+  ],
 };
