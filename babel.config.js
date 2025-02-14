66fcf00e2d74f55fc103e36cbd41559b3576dc7f
Tchasinga jacques <tchasingajacques@gmail.com>
2025-02-14 15:32:11 +0300

    refactor: Remove global.css, update babel.config.js for simplified configuration, and adjust tailwindcss version in package.json and package-lock.json

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,8 @@
+// babel.config.js
 module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: [
-        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-        "nativewind/babel",
-      ],
-    };
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+     plugins: ["nativewind/babel"],
   };
+};
