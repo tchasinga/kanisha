e55bb5c62d05ac02eed5362055d1248e53838594
Tchasinga jacques <tchasingajacques@gmail.com>
2025-02-14 15:06:04 +0300

    refactor: Update tailwind.config.js to include additional content paths and extend theme with custom font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",  // Include all JS, JSX, TS, and TSX files in the app folder
+    "./components/**/*.{js,jsx,ts,tsx}",  // Include all JS, JSX, TS, and TSX files in the components folder]
+    "./app/(tabs)/meditate.tsx"  
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rmono: ['Roboto-Mono', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
